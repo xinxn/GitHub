38ae69a59299b71bf566da1d1f2b8d33855281df
xinxn <2679044670@qq.com>
2018-10-26 16:19:17 +0800

    scened hello

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello{
 	public static void main(String[] args){
-		
+		for(int i=0;i<10;i++){
+			system.out.printLn("this is my first github"+i);
+		}
 		system.out.printLn("this is my first github");
 	}
 }
